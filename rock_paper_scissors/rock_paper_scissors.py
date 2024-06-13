bdf1bd424b643cf35f91d2d8663d036ae74d166e
Eyasir Ahamed <eyasir329@gmail.com>
2024-06-13 21:52:56 +0600

    update rock paper scissors

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,38 +1,48 @@
 import random
 import ascii_art
 
-value_img = [ascii_art.rock,ascii_art.paper,ascii_art.scissor]
+print("Welcome to Rock Paper Scissors")
+value_img = [ascii_art.rock, ascii_art.paper, ascii_art.scissor]
 # 0-rock, 1-paper, 2-scissor
 
-random_int = random.randint(0,2)
+random_int = random.randint(0, 2)
 input_key = input("Enter Rock as 'r', Paper as 'p', Scissor as 's' ")
-if input_key!='r' or input_key!='p' or input_key!='s':
+if input_key != 'r' or input_key != 'p' or input_key != 's':
     print("You have to choose right input")
-
-input_value = 0
-if input_key=='r': input_value=0
-elif input_key=='p': input_value=1
-else: input_value=2
-print(random_int)
-# game on
-name = ""
-if(input_value==0):name="Rock"
-elif(input_value==1):name="Paper"
-else:name="Scissor"
-
-print(f"You choose: {name}\n")
-print(value_img[input_value])
-
-if(random_int==0):name="Rock"
-elif(random_int==1):name="Paper"
-else:name="Scissor"
-print(f"\nComputer choose: {name}\n")
-print(value_img[random_int])
-
-if (input_value==0 and random_int==2) or (input_value==2 and random_int==1) or (input_value==2 and random_int==1):
-    print("You win ")
-elif input_value==random_int:
-    print("Draw")
 else:
-    print("You loose")
-    
+    input_value = 0
+    if input_key == 'r':
+        input_value = 0
+    elif input_key == 'p':
+        input_value = 1
+    else:
+        input_value = 2
+    print(random_int)
+    # game on
+    name = ""
+    if input_value == 0:
+        name = "Rock"
+    elif input_value == 1:
+        name = "Paper"
+    else:
+        name = "Scissor"
+
+    print(f"You choose: {name}\n")
+    print(value_img[input_value])
+
+    if random_int == 0:
+        name = "Rock"
+    elif random_int == 1:
+        name = "Paper"
+    else:
+        name = "Scissor"
+    print(f"\nComputer choose: {name}\n")
+    print(value_img[random_int])
+
+    if (input_value == 0 and random_int == 2) or (input_value == 2 and random_int == 1) or (
+            input_value == 2 and random_int == 1):
+        print("You win ")
+    elif input_value == random_int:
+        print("Draw")
+    else:
+        print("You loose")
